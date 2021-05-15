4c8e089ecbefa216d4c31b63c052dc56b2774339
thepresidentm <miguelanva9@gmail.com>
2021-05-14 20:23:50 -0500

    Validacion registro, se prohibieron caracteres y tamaño minimo de input

--- js/registro.js
+++ js/registro.js
@@ -1,19 +1,29 @@
 function validar(event){
+
     var correo = document.getElementById("inputCorreo").value;
     var contraseña = document.getElementById("inputContraseña").value;
     var usuario = document.getElementById("inputUsuario").value;
+
     var validacion1 = validacionCaracteresEspeciales(correo + contraseña + usuario);
     var validacion2 = validacionTextoVacio(correo, contraseña, usuario);
     var validacion3 = validacionEspacios(correo, contraseña, usuario);
+    var validacion4 = validacionTamaño(correo, contraseña, usuario);
 
-    if(validacion1==false | validacion2==false | validacion3 ==false){
-        event.preventDefault()
+    if(validacion1 == false | validacion2 == false | validacion3 == false | validacion4 == false){
+        event.preventDefault();
+    }
+}
+
+function validacionTamaño(input1, input2, input3){
+    if(input1.length < 8 | input2.length < 8 | input3.length < 8){
+        alert("Debes usar minimo 8 caracteres.");
+        return false;
     }
 }
 
 function validacionCaracteresEspeciales(input){
-    if(/"|'|,/.test(input)==true){
-        alert("No puedes usar comilla simple, comillas o comas.");
+    if(/"|'|,|<|>/.test(input)==true){
+        alert("No puedes usar comilla simple, comillas, comas o los simbolos mayor y menor que.");
         return false;
     }
 }
