2011a6174058826545aa6b254c94c59b30c95671
EMOA117 <emontesdeocaa1900@alumno.ipn.mx>
2021-05-19 21:18:53 -0500

    Carpetas assets

---
+++ Bearcareful/src/java/Modelo/Pais.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author EMOA1
+ */
+public class Pais {
+    private int id_country;
+    private String nom_country;
+    
+}
--- Bearcareful/src/java/Modelo/Usuario.java
+++ Bearcareful/src/java/Modelo/Usuario.java
@@ -5,10 +5,73 @@
  */
 package Modelo;
 
+import java.sql.Date;
+
 /**
  *
  * @author EMOA1
  */
 public class Usuario {
+    private int id_user, id_country;
+    private Date date_user;
+    private String nom_user, nom_account, pass_user, email ;
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public int getId_country() {
+        return id_country;
+    }
+
+    public void setId_country(int id_country) {
+        this.id_country = id_country;
+    }
+
+    public Date getDate_user() {
+        return date_user;
+    }
+
+    public void setDate_user(Date date_user) {
+        this.date_user = date_user;
+    }
+
+    public String getNom_user() {
+        return nom_user;
+    }
+
+    public void setNom_user(String nom_user) {
+        this.nom_user = nom_user;
+    }
+
+    public String getNom_account() {
+        return nom_account;
+    }
+
+    public void setNom_account(String nom_account) {
+        this.nom_account = nom_account;
+    }
+
+    public String getPass_user() {
+        return pass_user;
+    }
+
+    public void setPass_user(String pass_user) {
+        this.pass_user = pass_user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
     
 }
