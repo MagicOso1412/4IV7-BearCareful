97e62ba573ea53920910ad1c64e85eead38fbeb5
thepresidentm <miguelanva9@gmail.com>
2021-05-24 19:01:20 -0500

    Funcion inutil retirada

--- js/realizarTest.js
+++ js/realizarTest.js
@@ -1,12 +1,14 @@
 
 let input = "";
-let contador = 0;
-let validacion = true;
+/*let contador = 0;
+let validacion = true;*/
 
-// Esta funcion revisa si los radio button estan "checked", el bucle for se utiliza
-// para aprovechar que todos se llaman input+n
-// Cuando encuentra que un radio button no esta "checked" detiene el submit,
-// el bucle for y envia una alerta
+/*
+Esta funcion revisa si los radio button estan "checked", el bucle for se utiliza
+para aprovechar que todos se llaman input+n
+Cuando encuentra que un radio button no esta "checked" detiene el submit,
+el bucle for y envia una alerta
+*/
 
 function validar(event){
 
@@ -16,15 +18,19 @@ function validar(event){
         
         if(input == null){
             alert("Debes responder todas las preguntas");
-            validacion = false;
+            //validacion = false;
             event.preventDefault();
             break;
         } 
     }
+    /*
+    La seccion comentada pretendia sumar automaticamente los resultados
+    pero no funciona ya que se activa despues de que el formulario envie sus valores
     if(validacion==true){
         for (let i = 1; i < 22; i++){
             contador += parseInt(document.querySelector('input[name="input'+ i +'"]:checked').value);
         }
         document.getElementById("resultado").value = contador;
     }
+    */
 }
